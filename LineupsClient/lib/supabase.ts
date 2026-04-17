04fdff7dffc6009bacd70aa787b206650fb44bf3
Evan Romes <evanj.romes@gmail.com>
2026-04-16 22:20:49 -0500

    Move Supabase credentials to environment variables

--- LineupsClient/lib/supabase.ts
+++ LineupsClient/lib/supabase.ts
@@ -1,8 +1,8 @@
 import AsyncStorage from '@react-native-async-storage/async-storage'
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://yztxxqfnckvjvhpucifx.supabase.co'
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl6dHh4cWZuY2t2anZocHVjaWZ4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzUzMTYyMjQsImV4cCI6MjA5MDg5MjIyNH0.69TJD_nLWLp351_7HZktEzRjcyq1GrRGw9Eqv9K7Pik'
+const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL!
+const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY!
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
